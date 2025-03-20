563c9e056b5e2c91fd026bc1c792e4a7193b5bdd
Programming Rainbow <159774797+ProgrammingRainbow@users.noreply.github.com>
2025-03-20 10:34:44 +0000

    Add files via upload

---
+++ Video04/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SDL_FLAGS SDL_INIT_VIDEO
+
+#define WINDOW_TITLE "Random Colors"
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#endif
